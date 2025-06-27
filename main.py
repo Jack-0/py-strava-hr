c3e14809a0cbe17e93848ad3b716a93867b150c9
Jack <github.ay8yf@slmail.me>
2025-06-27 21:03:56 +0100

    init simple strava console app

---
+++ main.py
@@ -0,0 +1,87 @@
+import requests
+import webbrowser
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+AUTH_URL = "https://www.strava.com/oauth/authorize"
+TOKEN_URL = "https://www.strava.com/oauth/token"
+
+
+def get_authorization_code():
+    url = (
+        f"{AUTH_URL}?client_id={CLIENT_ID}&response_type=code"
+        f"&redirect_uri={REDIRECT_URI}&approval_prompt=force"
+        f"&scope=read,activity:read"
+    )
+    print(f"Opening browser to authorize app...\n{url}\n")
+    webbrowser.open(url)
+    code = input(
+        "🙈 Paste the `code` parameter from the URL you were redirected to: ").strip()
+    return code
+
+
+def exchange_token(code):
+    response = requests.post(TOKEN_URL, data={
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'code': code,
+        'grant_type': 'authorization_code'
+    })
+    response.raise_for_status()
+    return response.json()
+
+
+def get_athlete(access_token):
+    r = requests.get("https://www.strava.com/api/v3/athlete", headers={
+        'Authorization': f"Bearer {access_token}"
+    })
+    r.raise_for_status()
+    return r.json()
+
+
+def get_activities(access_token):
+    r = requests.get("https://www.strava.com/api/v3/athlete/activities", headers={
+        'Authorization': f"Bearer {access_token}"
+    }, params={'per_page': 100})
+    r.raise_for_status()
+    return r.json()
+
+
+def main():
+    code = get_authorization_code()
+    token_data = exchange_token(code)
+
+    access_token = token_data['access_token']
+    activities = get_activities(access_token)
+
+    filter_and_sort_by_heartrate(activities)
+
+
+def filter_and_sort_by_heartrate(items):
+    res = sorted(
+        [
+            {
+                "name": item.get("name"),
+                "id": item.get("id"),
+                "max_heartrate": item.get("max_heartrate")
+            }
+            for item in items
+            if "max_heartrate" in item and item.get("max_heartrate") is not None
+        ],
+        key=lambda x: x["max_heartrate"],
+        reverse=True
+    )
+    for i in range(3):
+        entry = res[i]
+        print(f"❤️ Max HR: {
+              entry['max_heartrate']}, URL: https://www.strava.com/activities/{entry['id']}")
+    return res
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Strava Max HR
+
+A really simple python app to get you max HR from last 100 activities
+
+## Setup and running
+
+1. Create an application. You will need to copy the client id and secret values
+    - https://www.strava.com/settings/api
+2. Copy .env-example to .env and populate the values
+3. Activate a virtual environment
+    - python 3 -m venv venv 
+        - Windows `venv\Scripts\activate`
+        - Linux/Mac `source venv\Scripts\activate`
+4. install dependencies `pip install -r requirements.txt`
+5. run `python main.py`
+6. You will now be redirected to a web-browser follow instructions printed in the terminal
